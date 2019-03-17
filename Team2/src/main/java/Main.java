8d89bcf7f3b414ba4cddc40a63fcc5fe3dd2dec1
SisiLiu61 <sliu6161@gmail.com>
2019-03-17 15:14:31 -0400

    Shiyi's commit

--- Team2/src/main/java/Main.java
+++ Team2/src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Collaborater name is " + "Jonathan1");
+        System.out.println("Shiyi Liu" + "Jonathan1");
     }
 }
