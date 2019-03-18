e4d1efd6333d031b044549ccb022d68e1270f40a
henryposkanzer <hposkanzer@gmail.com>
2019-03-18 13:13:20 -0400

    Deleted Collaborator is.

--- empty-project-2/src/main/java/Main.java
+++ empty-project-2/src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Collaborator is " + "Jonathan");
-        System.out.println("Collaborator is " + "Henry");
+        System.out.println("Henry");
     }
 }
