b2ff8fffc7648c04aba0d8a28c6d7bbabd950919
Panos <pargyrakis@wpi.edu>
2019-03-18 13:25:24 -0400

    Added Panos

--- empty-project-2/src/main/java/Main.java
+++ empty-project-2/src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Collaborator is " + "Jonathan");
         System.out.println("Collaborators are  " + "Henry and Ryan");
+        System.out.println("Panos");
     }
 }
