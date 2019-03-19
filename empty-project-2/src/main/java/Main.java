39277e9506ff344dd6027e144459619249511106
Ryan O'Brien <ryano647@gmail.com>
2019-03-19 13:09:24 -0400

    Forcing another conflict

--- empty-project-2/src/main/java/Main.java
+++ empty-project-2/src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Collaborator is " + "Jonathan");
-        System.out.println("Collaborators are  " + "Henry and Ryan");
+        System.out.println("Conflict here " + "Henry and Ryan");
         System.out.println("Panos");
     }
 }
