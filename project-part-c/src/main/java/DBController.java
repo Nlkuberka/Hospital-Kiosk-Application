eb76961d0988904917dfd29708275012613d4f77
Isabel Morales Sirgo <imoralessirgo@wpi.edu>
2019-03-26 12:48:12 -0400

    add node, delete node methods to DB

--- project-part-c/src/main/java/DBController.java
+++ project-part-c/src/main/java/DBController.java
@@ -12,7 +12,11 @@ public class DBController {
 
     }
 
-
+    /**
+     * DBConnect
+     *
+     * generates connection and table if non-existent
+     */
     public void DBConnect(){
         Connection connection = null;
         try {
@@ -27,27 +31,31 @@ public class DBController {
         }
     }
 
-
+    /**
+     * enterData
+     *
+     * enters node values to existing node table
+     * @param nodes list of nodes read from a CSV file
+     */
     public void enterData(LinkedList<Node> nodes){
         try {
             Connection connection = DriverManager.getConnection("jdbc:derby:myDB");
             Statement s = connection.createStatement();
             for (Node node : nodes) {
-                s.executeQuery("insert into NODES " +
-                        "values (" + node.getNodeID() + "," +
-                        " "+ node.getXcoord() +"," +
-                        " "+ node.getYcoord() +"," +
-                        " "+ node.getFloor() +"," +
-                        " "+ node.getBuilding() +"," +
-                        " "+ node.getNodeType() +"," +
-                        " "+ node.getLongName() +"," +
-                        " "+ node.getShortName() +")");
+            nodeInsert(s,node);
             }
             connection.close();
         }catch(SQLException e) {
             e.printStackTrace();
         }
     }
+
+    /**
+     * updateNode
+     *
+     * updates node of given id, overriding all fields
+     * @param node desired node content -- Must have an existing ID --
+     */
     public void updateNode(Node node){
         try{
             Connection connection = DriverManager.getConnection("jdbc:derby:myDB");
@@ -63,10 +71,72 @@ public class DBController {
                     "where nodeid=" + node.getNodeID()
             );
         }catch(SQLException e){
-
+            e.printStackTrace();
         }
     }
 
+    /**
+     * delete node
+     *
+     * deletes node of given ID
+     * @param ID of node to be deleted
+     */
+    public void deleteNode(String ID){
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:derby:myDB");
+            Statement s = conn.createStatement();
+            s.executeQuery("Delete from NODES where NODEID ="+ ID);
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * addNode
+     *
+     * lets user introduce a single node to the DB
+     * @param node new node object
+     */
+    public void addNode(Node node){
+       try{
+           Connection connection = DriverManager.getConnection("jdbc:derby:myDB");
+           Statement s = connection.createStatement();
+           nodeInsert(s,node);
+           connection.close();
+       }catch(SQLException e){
+           e.printStackTrace();
+       }
+    }
+
+    /**
+     * nodeInsert
+     *
+     * helper method, inserts nodes into existing table
+     * @param s existing statement
+     * @param node new node object
+     */
+    public void nodeInsert(Statement s, Node node){
+        try {
+            s.executeQuery("insert into NODES " +
+                    "values (" + node.getNodeID() + "," +
+                    " " + node.getXcoord() + "," +
+                    " " + node.getYcoord() + "," +
+                    " " + node.getFloor() + "," +
+                    " " + node.getBuilding() + "," +
+                    " " + node.getNodeType() + "," +
+                    " " + node.getLongName() + "," +
+                    " " + node.getShortName() + ")");
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * exportData
+     *
+     * selects all content held in Nodes table and prints it to a file
+     * @param filename name of output file
+     */
     public void exportData(String filename) {
         Connection connection = null;
         Statement stmt;
