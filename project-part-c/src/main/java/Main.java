344e140e7559e60e79bb2d75f52f86147d79cdc1
Isabel Morales Sirgo <imoralessirgo@wpi.edu>
2019-03-27 09:51:18 -0400

    fixed connection

--- project-part-c/src/main/java/Main.java
+++ project-part-c/src/main/java/Main.java
@@ -27,7 +27,7 @@ public class Main extends Application {
                 } catch (SQLException e) {
                     e.printStackTrace();
                 }
-                
+
             }
         });
     }
