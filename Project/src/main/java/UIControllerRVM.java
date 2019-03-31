0c5a505c7c43cee7359a78b524b4a7bd7014a4a0
Jon Chang <jonchang00work@gmail.com>
2019-03-31 15:28:35 -0400

    Stash

--- Project/src/main/java/UIControllerRVM.java
+++ Project/src/main/java/UIControllerRVM.java
@@ -4,12 +4,18 @@ import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
 
+import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Date;
+import java.util.Map;
+import java.util.HashMap;
 
 public class UIControllerRVM extends UIController {
+
+    @FXML
+    private Map<String, String> nodeIDs;
 
     @FXML
     private JFXButton confirmButton;
@@ -28,15 +34,19 @@ public class UIControllerRVM extends UIController {
 
     @FXML
     public void initalize() {
-
+        nodeIDs = new HashMap<String, String>();
+        //DB Get nodes
+        nodeSelect.getItems();
     }
 
     @FXML
     private void setConfirmButton() {
         LocalDate ld = datePicker.getValue();
         Instant instant = Instant.from(ld.atStartOfDay(ZoneId.systemDefault()));
-        Date startDate = Date.from(instant);
-        Date endDate = Date.from(instant);
+        Date date = Date.from(instant);
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+
+        Reservation r = new Reservation((String) nodeSelect.getValue())
 
         System.out.println(startTimePicker.getValue());
     }
