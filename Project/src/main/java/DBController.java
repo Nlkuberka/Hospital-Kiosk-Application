8886156e8c2a5ff56f99c1ba2727d917e149569a
Isabel Morales Sirgo <imoralessirgo@wpi.edu>
2019-03-31 17:05:21 -0400

    CREATE TABLE statments

--- Project/src/main/java/DBController.java
+++ Project/src/main/java/DBController.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -17,5 +20,52 @@ public class DBController {
         return null;
     }
 
+    public static void loadNodeData(File file, Connection connection){
+        BufferedReader br = null;
+        String line = "";
+        String[] arr;
+        try{
+            br = new BufferedReader(new FileReader(file));
+            br.readLine(); // skip header
+            while((line = br.readLine()) != null){
+                arr = line.split(",");
+                connection.createStatement().execute("insert into NODES" +
+                        "values ('"+ arr[0] +"',"+ arr[1]+","+ arr[2]+","+ arr[3]+",'"+ arr[4]+"','"+ arr[5]+"','"+ arr[6]+"','"+ arr[7]+"');");
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void loadEdgeData(File file, Connection connection){
+        BufferedReader br = null;
+        String line = "";
+        String[] arr;
+        try{
+            br = new BufferedReader(new FileReader(file));
+            br.readLine(); // skip header
+            while((line = br.readLine()) != null){
+                arr = line.split(",");
+                connection.createStatement().execute("insert into EDGES" +
+                        "values ('"+ arr[0] + "','"+ arr[1]+"','"+ arr[2]+"',);");
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void createTable(String createStatement, Connection conn){
+        try {
+            conn.createStatement().execute(createStatement);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    public static void loadEdgeData(File file){
+
+    }
 
 }
