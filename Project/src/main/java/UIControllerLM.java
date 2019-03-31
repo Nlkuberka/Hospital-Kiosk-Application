80182f5862208f5ca5c2c19b22bacddf1730aae6
Jon Chang <jonchang00work@gmail.com>
2019-03-31 13:43:25 -0400

    CurrentUser vs User fixes

--- Project/src/main/java/UIControllerLM.java
+++ Project/src/main/java/UIControllerLM.java
@@ -1,6 +1,18 @@
+import com.jfoenix.controls.JFXButton;
 import javafx.fxml.FXML;
 
+import javafx.scene.control.Button;
+
 public class UIControllerLM extends UIController {
+
+    @FXML
+    private JFXButton loginAsGuestButton; /**< The Login As Guest Button*/
+
+    @FXML
+    private JFXButton loginAsUserButton; /**< The Login As User Button*/
+
+    @FXML
+    private JFXButton loginAsAdminButton; /**< The Login As Admin Button*/
 
     public UIControllerLM() {
 
@@ -14,8 +26,30 @@ public class UIControllerLM extends UIController {
 
     }
 
+    /**
+     * Goes to the Guest Main Menu
+     */
     @FXML
     private void setLoginAsGuestButton() {
-        this.goToScene(UIController.MAIN_MENU_MAIN);
+        CurrentUser.permissions = User.GUEST_PERMISSIONS;
+        this.goToScene(UIController.GUEST_MAIN_MENU_MAIN);
+    }
+
+    /**
+     * Goes to the User Main Menu
+     */
+    @FXML
+    private void setLoginAsUserButton() {
+        CurrentUser.permissions = User.BASIC_PERMISSIONS;
+        this.goToScene(UIController.BASIC_MAIN_MENU_MAIN);
+    }
+
+    /**
+     * Goes to the Admin Main Menu
+     */
+    @FXML
+    private void setLoginAsAdminButton() {
+        CurrentUser.permissions = User.ADMIN_PERMISSIONS;
+        this.goToScene(UIController.ADMIN_MAIN_MENU_MAIN);
     }
 }
