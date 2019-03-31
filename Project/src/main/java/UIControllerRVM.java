27af104e1ed8d26315bd97702e0f444544c33c0f
Jon Chang <jonchang00work@gmail.com>
2019-03-31 17:14:11 -0400

    RV Controller and UI Linked

--- Project/src/main/java/UIControllerRVM.java
+++ Project/src/main/java/UIControllerRVM.java
@@ -9,6 +9,7 @@ import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
 
@@ -33,10 +34,14 @@ public class UIControllerRVM extends UIController {
     private JFXTimePicker endTimePicker;
 
     @FXML
-    public void initalize() {
+    public void initialize() {
         nodeIDs = new HashMap<String, String>();
+        List<Node> nodes;
         //DB Get nodes
-        nodeSelect.getItems();
+        /*for(int i = 0; i < nodes.size(); i++) {
+            nodeIDs.put(nodes.get(i).getShortName(), nodes.get(i).getNodeID());
+            nodeSelect.getItems().add(nodes.get(i).getShortName());
+        }*/
     }
 
     @FXML
@@ -46,7 +51,8 @@ public class UIControllerRVM extends UIController {
         Date date = Date.from(instant);
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 
-        Reservation r = new Reservation((String) nodeSelect.getValue(), CurrentUser.userID, format.format(date), startTimePicker.getValue() + ":00", endTimePicker.getValue() + ":00");
+        Reservation r = new Reservation(nodeIDs.get((String) nodeSelect.getValue()), CurrentUser.userID, format.format(date), startTimePicker.getValue() + ":00", endTimePicker.getValue() + ":00");
         // DB Send
+        System.out.println(date);
     }
 }
