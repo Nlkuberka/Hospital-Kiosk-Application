bf6c6aba19237152559994b3555aca64913760a0
Nicole Kuberka <nlkuberka@wpi.edu>
2019-03-31 14:11:49 -0400

    database, node table, and edge table setup

--- Project/src/main/java/Main.java
+++ Project/src/main/java/Main.java
@@ -11,6 +11,27 @@ public class Main extends Application {
         controller.goToScene(UIController.ADMIN_TOOLS_VIEW_NODES);
 
         System.out.println("Collaborator is " + "X");
+
+        /**Connection C = DBController.dbConnect();
+         C.createStatement().execute("CREATE TABLE EDGES ("
+         + "EDGEID VARCHAR(21),"
+         + "STARTNODE VARCHAR(10),"
+         + "ENDNODE varchar(10),"
+         + "CONSTRAINT EDGE_PK PRIMARY KEY(EDGEID))"
+         );
+
+         C.createStatement().execute("CREATE TABLE NODES("
+         + "NODEID VARCHAR(10),"
+         + "XCOORD INTEGER,"
+         + "YCOORD INTEGER,"
+         + "FLOOR INTEGER,"
+         + "BUILDING VARCHAR(15),"
+         + "NODETYPE VARCHAR(4),"
+         + "LONGNAME VARCHAR(50),"
+         + "SHORTNAME VARCHAR(25),"
+         + "CONSTRAINT NODE_PK PRIMARY KEY(NODEID))"
+         );
+         **/
     }
 
     public static void main(String[] args) {
