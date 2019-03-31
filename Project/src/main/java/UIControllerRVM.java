b2a1fef7d292f5af3a24b69e012d6aa92274821f
Jon Chang <jonchang00work@gmail.com>
2019-03-31 13:40:02 -0400

    UI Controller Setup

---
+++ Project/src/main/java/UIControllerRVM.java
@@ -0,0 +1,33 @@
+import com.jfoenix.controls.JFXTimePicker;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+
+public class UIControllerRVM extends UIController {
+
+    @FXML
+    private Button confirmButton;
+
+    @FXML
+    private ChoiceBox nodeSelect;
+
+    @FXML
+    private DatePicker datePicker;
+
+    @FXML
+    private JFXTimePicker startTimePicker;
+
+    @FXML
+    private JFXTimePicker endTimePicker;
+
+    @FXML
+    public void initalize() {
+
+    }
+
+    @FXML
+    private void setConfirmButton() {
+
+    }
+}
