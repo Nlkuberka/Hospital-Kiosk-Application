02273f3ea1e4fd839dc4d4c599b392dca014dc7b
Ryan O'Brien <ryano647@gmail.com>
2019-04-01 23:29:28 -0400

    Fetch node and node list working

--- Project/src/main/java/Node.java
+++ Project/src/main/java/Node.java
@@ -86,16 +86,17 @@ public class Node {
         this.shortName = shortName;
     }
 
+    @Override
     public String toString() {
-        String returnValue = "Node(";
-        returnValue += getNodeID() + ", ";
-        returnValue += getXcoord() + ", ";
-        returnValue += getYcoord() + ", ";
-        returnValue += getFloor() + ", ";
-        returnValue += getBuilding() + ", ";
-        returnValue += getNodeType() + ", ";
-        returnValue += getLongName() + ", ";
-        returnValue += getShortName() + ")";
-        return  returnValue;
+        return "Node{" +
+                "nodeID='" + nodeID + '\'' +
+                ", xcoord=" + xcoord +
+                ", ycoord=" + ycoord +
+                ", floor='" + floor + '\'' +
+                ", building='" + building + '\'' +
+                ", nodeType='" + nodeType + '\'' +
+                ", longName='" + longName + '\'' +
+                ", shortName='" + shortName + '\'' +
+                '}';
     }
 }
