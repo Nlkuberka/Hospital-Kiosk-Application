566683d6078f0f37de8d6877290633a832c1e308
henryposkanzer <hposkanzer@gmail.com>
2019-04-07 13:58:23 -0400

    Added comments.

--- Project/src/main/java/application/Encryptor.java
+++ Project/src/main/java/application/Encryptor.java
@@ -1,6 +1,7 @@
 package application;
 
 public class Encryptor {
+    // A matrix that encrypts blocks of 5 characters
     private static final int[][] ENCRYPTION_MATRIX = new int[][] {
             {149, 146,   0, 224, 170},
             {171, 135, 107, 122, 112},
@@ -10,15 +11,17 @@ public class Encryptor {
     };
     public static final int MAX_PASSWORD_LENGTH = 15;
     public static String encrypt(String password) {
-        if(password.length() > 15) {
+        if(password.length() > 15) {    // if the input exceeds maximum password length
             return null;
         }
+        // Pad the input with spaces until it is 15 characters long.
         while(password.length() < 15) {
             password = password + ' ';
         }
-        String cipherText = "";
-        for(int block = 0; block < MAX_PASSWORD_LENGTH; block += ENCRYPTION_MATRIX.length) {
+        String cipherText = ""; // the encrypted password
+        for(int block = 0; block < MAX_PASSWORD_LENGTH; block += ENCRYPTION_MATRIX.length) {    // for each block of five characters
             String substring = password.substring(block, block + ENCRYPTION_MATRIX.length);
+            // Multiply the encryption matrix by the substring (interpreted as a vector of integers) (mod 256) to get a new string of length 5.
             for(int i = 0; i < ENCRYPTION_MATRIX.length; i++) {
                 char encryptedChar = 0;
                 for(int j = 0; j < ENCRYPTION_MATRIX.length; j++) {
