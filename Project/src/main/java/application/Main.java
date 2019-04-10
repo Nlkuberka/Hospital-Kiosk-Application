e262922f0ce0de32ead488a351d303464362c829
Dimitri Berardi <dberardi@wpi.edu>
2019-04-10 06:14:24 -0400

    Final

--- Project/src/main/java/application/Main.java
+++ Project/src/main/java/application/Main.java
@@ -11,10 +11,18 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         UIController controller = new UIController(primaryStage);
 
+<<<<<<< Updated upstream
         controller.goToScene(UIController.LOGIN_MAIN);
 
         System.out.println("Collaborator is " + "X");
         CurrentUser.currentAlgorithm = CurrentUser.AALOGRITHM;
+=======
+        controller.goToScene(UIController.ADMIN_TOOLS_MAIN);
+
+
+        System.out.println("Collaborator is " + "X");
+
+>>>>>>> Stashed changes
         //DBController.initializeAppDB();
     }
 
