2f458ee900c1e991fbead3765898c9726d7e71a8
RVeets134 <rakesh.veetekat@gmail.com>
2019-04-14 17:28:04 -0400

    Created pop-up and UIController for selected node options.

---
+++ Project/src/main/java/admintools/UIControllerPUMVNO.java
@@ -0,0 +1,10 @@
+package admintools;
+
+import application.UIController;
+import javafx.scene.layout.StackPane;
+
+public class UIControllerPUMVNO extends UIController {
+
+    public StackPane parentPane;
+
+}
