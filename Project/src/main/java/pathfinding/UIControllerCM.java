921739d14a8110f920feb3a5050132206ceb83fa
SisiLiu61 <sliu6161@gmail.com>
2019-04-15 15:58:46 -0400

    context_menu

---
+++ Project/src/main/java/pathfinding/UIControllerCM.java
@@ -0,0 +1,36 @@
+package pathfinding;
+
+import application.UIController;
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class UIControllerCM extends UIController {
+    @FXML
+    private JFXButton startButton;
+    @FXML
+    private JFXButton destinationButton;
+    @FXML
+    private Stage stage;
+    @FXML
+    private Scene scene;
+
+    @FXML
+    public void initialize() {
+        stage.initStyle(StageStyle.TRANSPARENT);
+        scene.setFill(Color.TRANSPARENT);
+    }
+
+    @FXML
+    public void setStartButton(){
+
+    }
+
+    @FXML
+    public void setDestinationButton(){
+
+    }
+}
