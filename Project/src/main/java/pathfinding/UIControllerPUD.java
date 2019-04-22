890f6f7b4db14ce595dd56cf44937c99a61d44cf
Joseph <Joseph>
2019-04-21 20:54:34 -0400

    added combo box to select floor

--- Project/src/main/java/pathfinding/UIControllerPUD.java
+++ Project/src/main/java/pathfinding/UIControllerPUD.java
@@ -2,6 +2,7 @@ package pathfinding;
 
 import application.UIController;
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXToggleButton;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
@@ -37,12 +38,16 @@ public class UIControllerPUD extends UIController {
     @FXML
     private JFXButton okButton; //ok button to exit confirmation window
     @FXML
-    JFXToggleButton noStairsButton;
+    private JFXComboBox floorSelect; //drop down to select which floor's directions to display
 
-
+    @FXML
+    public void onShow(){
+        floorSelect.getItems().addAll("All", "L2", "L1", "G", "1", "2", "3", "4");
+    }
     @FXML
     public void setDirections(List<List<List<String>>> message) {
         String returnText = "";
+
         for(int i = 0; i < message.size(); i++){
             for (int j = 0; j < message.get(i).size(); j++) {
                 for (int k = 0; k < message.get(i).get(j).size(); k++) {
