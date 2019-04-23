80f47d2f10cda96ea970fd1affd9d8ad6aa0b678
Jon Chang <jonchang00work@gmail.com>
2019-04-23 16:43:51 -0400

    Thread Timout Controller

--- Project/src/main/java/admintools/UIControllerATAS.java
+++ Project/src/main/java/admintools/UIControllerATAS.java
@@ -1,5 +1,6 @@
 package admintools;
 
+import application.SessionTimeoutThread;
 import application.UIController;
 import com.jfoenix.controls.JFXButton;
 import javafx.fxml.FXML;
@@ -24,5 +25,26 @@ public class UIControllerATAS extends UIController {
     @FXML
     private MenuItem backMenuItem;
 
+    @FXML
+    public void initialize() {
 
+    }
+
+    @FXML
+    public void onShow() {
+        long timeout = SessionTimeoutThread.timeout;
+        long seconds = timeout / 1000;
+        minutLabel.setText(seconds / 60 + "min");
+        secondLabel.setText(seconds % 60 + "s");
+    }
+
+    @FXML
+    public void setBackMenuItem() {
+        this.goToScene(UIController.ADMIN_TOOLS_MAIN);
+    }
+
+    @FXML
+    public void setConfirmButton() {
+        
+    }
 }
