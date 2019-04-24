06f2bc5c8fe62c750a16d4e01765053875b79476
Jon Chang <jonchang00work@gmail.com>
2019-04-23 23:32:58 -0400

    Fixed stuff

--- Project/src/main/java/admintools/UIControllerATAS.java
+++ Project/src/main/java/admintools/UIControllerATAS.java
@@ -1,13 +1,10 @@
 package admintools;
 
-import application.SessionTimeoutThread;
 import application.UIController;
 import com.jfoenix.controls.JFXButton;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
-import javafx.scene.control.MenuItem;
 
 public class UIControllerATAS extends UIController {
     @FXML
