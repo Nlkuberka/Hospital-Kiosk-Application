b63080bef58e3628c8fc427626040ca0e9ce1f7c
Isabel Morales Sirgo <imoralessirgo@wpi.edu>
2019-04-26 03:06:19 -0400

    network on hold

--- Project/src/main/java/application/Main.java
+++ Project/src/main/java/application/Main.java
@@ -44,8 +44,9 @@ public class Main extends Application {
         }
 
         CurrentUser.network = new DBNetwork(serverSocketNum, clientSocketNum);
-        //CurrentUser.network.hold();
-//        CurrentUser.network.mute();
+        CurrentUser.network.hold();
+
+        CurrentUser.network.mute();
 
         System.out.println(DBNetwork.ipAddresses);
 
